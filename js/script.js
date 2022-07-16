48299040d68b57bd21a52006c2c8e2d9d336bd88
Elisha <elishageorge111@gmail.com>
2022-07-16 17:01:34 +0500

    js completed for counter

--- js/script.js
+++ js/script.js
@@ -1,36 +1,81 @@
-let btnIncrease =document.getElementById("increase");
-let btnDecrease =document.getElementById("decrease");
-let btnReset =document.getElementById("reset");
+// let btnIncrease =document.getElementById("increase");
+// let btnDecrease =document.getElementById("decrease");
+// let btnReset =document.getElementById("reset");
 
-let countDisplay=document.getElementById("count");
-let count=0;
+// let countDisplay=document.getElementById("count");
+// let count=0;
 
 
-btnIncrease.addEventListener('click', function()
+// btnIncrease.addEventListener('click', function()
+// {
+
+//     count ++;
+//     countDisplay.style.color="green";
+//     countDisplay.textContent=count;
+
+// })
+
+
+// btnDecrease.addEventListener('click', function()
+// {
+
+//     count --;
+//     countDisplay.style.color="red";
+//     countDisplay.textContent=count;
+
+// })
+
+// btnReset.addEventListener('click', function()
+// { 
+
+//     count=0;
+//     countDisplay.style.color="purple";
+//     countDisplay.textContent=count;
+// })
+// ---------------------VERY BASIC CODE------------
+
+let counter=0;
+let value=document.getElementById("count");
+let btn=document.querySelectorAll(".btns");
+
+btn.forEach(function(btns)
 {
-
-    count ++;
-    countDisplay.style.color="green";
-    countDisplay.textContent=count;
-
-})
-
-
-btnDecrease.addEventListener('click', function()
-{
-
-    count --;
-    countDisplay.style.color="red";
-    countDisplay.textContent=count;
-
-})
-
-btnReset.addEventListener('click', function()
-{ 
-
-    count=0;
-    countDisplay.style.color="purple";
-    countDisplay.textContent=count;
     
+// it will fetch values in a npde list 
+
+btns.addEventListener('click', function(e)
+{
+    // e object of event
+
+    const styles=e.currentTarget.classList;
+    console.log(styles)
+    if(styles.contains("decrease"))
+    {
+        counter--;
+    }
+    else if(styles.contains("increase"))
+    {
+        counter++;
+    }
+    else
+    {
+        counter=0;
+    }
+
+    if(counter < 0)
+    {
+        value.style.color='red';
+    }
+    if(counter > 0)
+    {
+        value.style.color='green';
+    } if(counter == 0)
+    {
+        value.style.color='purple';
+    }
+    value.textContent=counter;
+    console.log(counter)
+
+})
 
 })
